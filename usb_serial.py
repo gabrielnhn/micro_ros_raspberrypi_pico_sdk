4b11369f3535ee9823a334c0436e5d555daac3c8
Gabriel Nascarella <ielnascarella@hotmail.com>
2022-06-24 02:14:09 -0300

    add usb serial

---
+++ usb_serial.py
@@ -0,0 +1,13 @@
+import serial
+
+ser = serial.Serial('/dev/ttyACM0')  # open serial port
+print(ser.name)         # check which port was really used
+
+while True:
+    
+    ser.write(b'140 20\n')     # write a string
+    # a = ser.read_until(b'\n')
+
+    # print(a)
+
+ser.close()     
